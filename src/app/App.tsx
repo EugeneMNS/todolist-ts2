440ec26a4a7c87e25ed920bb960dd825448469c0
EugeneMNS <evgen.str4@gmail.com>
2022-04-27 17:15:57 +0300

    16 - Login, Router, App initializing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './App.css'
 import { TodolistsList } from '../features/TodolistsList/TodolistsList'
-import { useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { AppRootStateType } from './store'
-import { RequestStatusType } from './app-reducer'
+import {initializeAppTC, RequestStatusType} from './app-reducer'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -13,13 +13,44 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import { Menu } from '@mui/icons-material';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/Login/Login";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import {CircularProgress} from "@mui/material";
+import {logoutTC} from "../features/Login/auth-reducer";
 
 type PropsType = {
     demo?: boolean
 }
 
 function App({demo = false}: PropsType) {
+
+    const dispatch = useDispatch()
     const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>((state) => state.auth.isLoggedIn)
+
+    useEffect(()=>{
+        dispatch(initializeAppTC())
+    },[])
+
+    if (!isInitialized) {
+        return (
+            //<LinearProgress/>
+        <div style={{position: 'fixed', top: '30%', textAlign:'center', width:'100%'}}>
+            <CircularProgress/>
+        </div>
+        )
+    }
+
+    const logoutHandler = () => {
+        dispatch(logoutTC())
+    }
+
     return (
         <div className="App">
             <ErrorSnackbar/>
@@ -31,12 +62,20 @@ function App({demo = false}: PropsType) {
                     <Typography variant="h6">
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+
+                    {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Logout</Button>}
+
+
                 </Toolbar>
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList demo={demo}/>
+                <Routes>
+                    <Route path="/" element={<TodolistsList demo={demo}/>}/>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="/404" element={<h2>404: PAGE NOT FOUND</h2>}/>
+                    <Route path="*" element={<Navigate to="/404"/>}/>
+                </Routes>
             </Container>
         </div>
     )
